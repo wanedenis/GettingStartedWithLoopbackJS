8c97e0a2e3e679befbdf31a40fb3f6250f2c3f42
denis wane <wanedenis3@gmail.com>
2020-01-14 10:16:55 +0100

    set mongodb

--- common/models/user.js
+++ common/models/user.js
@@ -7,7 +7,7 @@ var config = require('../../server/config.json');
 var path = require('path');
 
 //Replace this address with your actual address
-var senderAddress = 'noreply@loopback.com'; 
+var senderAddress = 'wanedenis3@gmail.com';
 
 module.exports = function(User) {
   //send verification email after registration
